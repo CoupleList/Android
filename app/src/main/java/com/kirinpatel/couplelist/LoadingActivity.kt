019a06cadaaa818ac237cf04eb6d9c62047cf2c3
Kirin Patel <kirinpatel@gmail.com>
2018-10-08 12:22:32 -0400

    Started work on basic authentication flow

--- app/src/main/java/com/kirinpatel/couplelist/LoadingActivity.kt
+++ app/src/main/java/com/kirinpatel/couplelist/LoadingActivity.kt
@@ -1,12 +1,26 @@
 package com.kirinpatel.couplelist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LoadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
