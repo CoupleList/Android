ec2d7178ec8df1f5f228ebf1875c09af4c276914
Kirin Patel <kirinpatel@gmail.com>
2018-10-08 23:12:06 -0400

    Started work on list generation/join abilities

---
+++ app/src/main/java/com/kirinpatel/couplelist/GenerateListActivity.kt
@@ -0,0 +1,12 @@
+package com.kirinpatel.couplelist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GenerateListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_list)
+    }
+}
--- app/src/main/java/com/kirinpatel/couplelist/LoadingActivity.kt
+++ app/src/main/java/com/kirinpatel/couplelist/LoadingActivity.kt
@@ -4,23 +4,110 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class LoadingActivity : AppCompatActivity() {
+
+    private lateinit var mDatabase: DatabaseReference
+    private lateinit var uid: String
+
+    private val userListener = object : ValueEventListener {
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            if (dataSnapshot.exists()) {
+                // User node exists in Firebase
+                if (dataSnapshot.hasChild("list")) {
+                    // Version 2 of list node exists in user node
+                    startMainActivity()
+                } else {
+                    // Check if user has version 1 of list node in user node
+                    dataSnapshot.children.forEach { child ->
+                        if (child.hasChild("code") && !child.hasChild("key")) {
+                            // Version 1 of list node is in user node
+                            upgradeListVersion(
+                                    child.child("key").value.toString(),
+                                    child.child("code").value.toString())
+                        }
+                    }
+                }
+            } else {
+                // User node does no exist in Firebase
+                startGenerateListActivity()
+            }
+        }
+
+        override fun onCancelled(databaseError: DatabaseError) {
+            // TODO: Handle error
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        mDatabase = FirebaseDatabase.getInstance().reference
     }
 
     override fun onResume() {
         super.onResume()
 
-        if (FirebaseAuth.getInstance().currentUser == null) {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user == null) {
+            startLoginActivity()
         } else {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            uid = user.uid
+            mDatabase
+                    .child("users")
+                    .child(uid)
+                    .addListenerForSingleValueEvent(userListener)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (uid.isNotEmpty()) {
+            mDatabase
+                    .child("users")
+                    .child(uid)
+                    .removeEventListener(userListener)
+
+            uid = ""
+        }
+    }
+
+    private fun startLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun startGenerateListActivity() {
+        val intent = Intent(this, GenerateListActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun upgradeListVersion(key: String, code: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null) {
+            // Set new list information
+            val listData: HashMap<String, Any> = HashMap()
+            listData["key"] = key
+            listData["code"] = code
+            mDatabase
+                    .child("user")
+                    .child(user.uid)
+                    .child("list")
+                    .setValue(listData)
+                    .addOnSuccessListener {
+                        // Remove old list information
+                        mDatabase.child("user").child(user.uid).child(key).removeValue()
+                    }
         }
     }
 }
